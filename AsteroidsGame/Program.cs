cd1272cfef5e773d30af30b3526043c87674e7a9
Mateusz Podeszwa <podinatubie@gmail.com>
2025-11-24 01:10:50 +0000

    Reseting the Main() It is my usual Main() method reset, as it is going to be used only to start up the application

--- AsteroidsGame/Program.cs
+++ AsteroidsGame/Program.cs
@@ -1,8 +1,8 @@
 ﻿namespace AsteroidsGame;
 
-class Program
+internal static class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
     }
