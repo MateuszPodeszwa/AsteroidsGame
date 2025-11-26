4e573c63b64c24038e38eee0d797ee081dedc0a3
Mateusz Podeszwa <podinatubie@gmail.com>
2025-11-26 23:46:33 +0000

    Temp. HashSet of ConsoleKeys for Debugging

---
+++ AsteroidsGame/Core/Game.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using AsteroidsGame.Engine.Input;
+
+namespace AsteroidsGame;
+
+public class Game
+{
+    // Define List of allowed keystrokes
+    internal static readonly HashSet<ConsoleKey> AllowedKeys =
+    [
+        ConsoleKey.W,
+        ConsoleKey.S,
+        ConsoleKey.A,
+        ConsoleKey.D,
+        ConsoleKey.UpArrow,
+        ConsoleKey.DownArrow,
+        ConsoleKey.LeftArrow,
+        ConsoleKey.RightArrow,
+        ConsoleKey.Spacebar,
+        ConsoleKey.Z,
+        ConsoleKey.X,
+        ConsoleKey.C,
+        ConsoleKey.Escape,
+        ConsoleKey.Enter
+    ];
+    
+
+    internal Game()
+    {
+        
+    }
+}
