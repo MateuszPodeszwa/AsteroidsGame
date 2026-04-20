72739f797228cad389cb83e528e54f934262ed5e
Mateusz Podeszwa <podinatubie@gmail.com>
2026-04-20 22:15:35 +0100

    Cleanup Main()

--- AsteroidsGame/Program.cs
+++ AsteroidsGame/Program.cs
@@ -1,6 +1,4 @@
-﻿// ReSharper disable UnusedParameter.Local
-
-using System;
+﻿using System;
 using Spectre.Console;
 
 namespace AsteroidsGame;
@@ -9,31 +7,6 @@ internal static class Program
 {
     private static void Main(string[] args) //
     {
-        // Create the layout
-        var layout = new Layout("Root").SplitRows(
-            new Layout("Top"),
-            new Layout("Bottom"));
-
-        // Update the left column
-        layout["Top"].Update(new Panel
-            (
-                Align.Left
-                (
-                    new Markup($"Score: 898" ), 
-                    VerticalAlignment.Middle
-                )).Expand()).Size(5);
         
-        layout["Bottom"].Update(new Panel
-        (
-            Align.Center
-            (
-                new Markup("I(^)I"), 
-                VerticalAlignment.Bottom
-            )).Expand());
-
-        // Render the layout
-        AnsiConsole.Write(layout);
-        Console.Out.Flush();
-        Console.ReadLine();
     }
 }
