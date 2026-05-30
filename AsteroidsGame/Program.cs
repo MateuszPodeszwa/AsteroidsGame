215bff749bd027c9675fe81ea83770b68fd4c1dc
Mateusz Podeszwa <podinatubie@gmail.com>
2026-05-30 13:22:26 +0100

    Implement game hosting service and refactor game execution logic
    Signed-off-by: Mateusz Podeszwa <podinatubie@gmail.com>

--- AsteroidsGame/Program.cs
+++ AsteroidsGame/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using Spectre.Console;
+﻿using System.Threading.Tasks;
+using AsteroidsGame.Core;
+using AsteroidsGame.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace AsteroidsGame;
 
 internal static class Program
 {
-    private static void Main(string[] args) //
+    private static async Task Main(string[] args) //
     {
+        HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
         
+        builder.Services.AddHostedService<GameHostedService>();
+        builder.Services.AddSingleton<Game>();
+        
+        IHost app = builder.Build();
+        await app.RunAsync();
     }
 }
---
+++ AsteroidsGame/Services/GameHostedService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AsteroidsGame.Core;
+using Microsoft.Extensions.Hosting;
+
+namespace AsteroidsGame.Services;
+
+public class GameHostedService(Game game) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        try
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await game.Run(stoppingToken);
+                Console.WriteLine($"Game running at {DateTime.Now}");
+            }
+        }
+        catch (Exception ex) when (ex is not OperationCanceledException)
+        {
+            Console.WriteLine($"Something went wrong, {ex.Message}");
+        }
+        finally
+        {
+            Console.WriteLine($"Game stopped at {DateTime.Now}");
+            game.Stop();
+        }
+    }
+}
